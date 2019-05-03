10fab1ea7148fdb6fb6f4519af6f03215e4be61f
Gomes <gomes.webads@gmail.com>
2019-05-03 11:12:17 -0300

    Alteração de layout do projeto. Em andamento.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,6 @@ class HomeController
     public function index()
     {
         $view = new View('site/index.phtml', true);
-        $view->users = (new ModelUser())->findAll();
         return $view->render();
     }
 }
