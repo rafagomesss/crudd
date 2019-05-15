90f6a4792efb07c0592efd200fb36603c7932d3c
Gomes <gomes.webads@gmail.com>
2019-05-15 16:44:29 -0300

    Confirmação para exclusão de despesas

--- public/assets/js/expense/expense.js
+++ public/assets/js/expense/expense.js
@@ -1,32 +1,47 @@
 $(document).ready(function() {
     $('#value').mask("#.##0,00", {reverse: true});
     $('.btnExpenseDelete').on('click', function() {
-        $.ajax({
-            url: '/expense/delete',
-            type: 'POST',
-            dataType: 'JSON',
-            data: {id: $(this).data('expense-delete')}
-        }).done(function(data) {
-            let message = data.message;
-            let classes = 'success';
-            let redirect = '/expense/list';
-            if (data.erro) {
-                classes = 'warning';
-                redirect = '';
-                switch (data.code) {
-                    case '23000':
-                    message = 'O e-mail informado já existe!';
-                    break;
-                    default:
-                    message = data.message;
-                    break;
+        Swal.fire({
+           title: 'Deseja excluir essa despesa?',
+           text: '* Essa operação não pode ser desfeita!',
+           type: 'warning',
+           showCancelButton: true,
+           confirmButtonColor: '#30D6A1',
+           cancelButtonColor: '#EF4747',
+           confirmButtonText: 'Sim, excluir!',
+           customClass: {
+              content: 'font-size-sm text-danger font-weight-bold',
+          }
+       }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                url: '/expense/delete',
+                type: 'POST',
+                dataType: 'JSON',
+                data: {id: $(this).data('expense-delete')}
+            }).done(function(data) {
+                let message = data.message;
+                let classes = 'success';
+                let redirect = '/expense/list';
+                if (data.erro) {
+                    classes = 'warning';
+                    redirect = '';
+                    switch (data.code) {
+                        case '23000':
+                        message = 'O e-mail informado já existe!';
+                        break;
+                        default:
+                        message = data.message;
+                        break;
+                    }
                 }
-            }
-            configModalAlert(message, classes, redirect);
-        }).fail(function() {
-            configModalAlert('Ocorreu um erro ao salvar o registro!', 'error');
-        });
+                configModalAlert(message, classes, redirect);
+            }).fail(function() {
+                configModalAlert('Ocorreu um erro ao salvar o registro!', 'error');
+            });
+        }
     });
+   });
 
     $('#btnFormExpenseEdit').on('click', function() {
         const form = $("#formExpenseEdit");
